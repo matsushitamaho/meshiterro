7fe787ae4c808392291d8ee7ee452a4153f0a317
mathushitamaho <132318410+matsushitamaho@users.noreply.github.com>
2023-05-13 16:03:41 +0000

    [Finish]4-13

--- app/controllers/postimages_controller.rb
+++ app/controllers/postimages_controller.rb
@@ -18,6 +18,12 @@ class PostimagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
   end
   
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to postimages_path
+  end
+  
   private
   
   def post_image_params
